125f82e4546e93c110e813640b0c702097cd6aa5
Jim Bolding <freebold1984@users.noreply.github.com>
2025-10-31 10:11:43 -0500

    Remove supply-chain-attack directory for public compliance

--- supply-chain-attack/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  formatBookingDate: (date) => new Date(date).toISOString(),
-  calculateNights: (checkIn, checkOut) => Math.ceil((checkOut - checkIn) / (1000 * 60 * 60 * 24)),
-  validateRoomType: (type) => ['standard', 'deluxe', 'suite'].includes(type)
-};
